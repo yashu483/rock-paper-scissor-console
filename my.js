34cacedf2dbf5851920b9087912426ece5462f7b
Yashu Lilhare <182187926+yashu483@users.noreply.github.com>
2024-10-29 17:16:57 +0530

    Complete single round stage

--- my.js
+++ my.js
@@ -1,97 +1,105 @@
 "use strict";
 
-console.log("Hellow, JavaScript check")
+console.log(`Lets play ROCK-PAPER-SCISSOR
+    `);
 
-// 1. programme will choose a ramdom option between rock paper and scissor
-//     1. programme will choose for a random number 
 
-let rock;
-let paper;
-let scissor;
+let computerSelection;
+let computerScore = 0;
+let humanScore = 0;
 
 function getComputerChoice() {
+
     let randomNumber = Math.floor(Math.random() * 100);
-    let choice;
+
     if (randomNumber < 33) {
-        choice = "ROCK";
-
-    } else if (randomNumber < 66) {
-        choice = "PAPER";
+        computerSelection = "ROCK"
     }
-    else {
-        choice = "SCISSOR";
-    };
-
-    return choice;
+    else if (randomNumber < 66) {
+        computerSelection = "PAPER"
+    }
+    else if (randomNumber < 100) {
+        computerSelection = "SCISSOR"
+    }
+    return computerSelection;
 };
+computerSelection = getComputerChoice();
 
-getComputerChoice();
+let humanSelection = prompt("Write your choice between rock, paper and scissor",).toUpperCase();
 
-console.log(getComputerChoice());
+// console.log(humanSelection)
 
-//     2. multiplying random number by 100 converting it into 1 to hundred floting numer
-//     3. converting floting number to a nearst integer
-//     4. if number is between 0 to 33 then itll denote rock
-
-//     5. if number is between 33 to 66 then itll denote paper 
-//     6. if number is between 66 to 100 then itll denote scissor
-// 2. User will give one option among these three and itll be stored in a variable.
-
-let getHumanChoice = prompt("Write your choice between rock , paper , scissor", "");
-let gett = getHumanChoice.toUpperCase();
-console.log(gett);
-
-let humanScore = 0;
-let computerScore = 0;
+// completion of input stage
 
 function playRound(humanChoice, computerChoice) {
-    String(humanChoice);
-    // humanChoice.toUppeCase();
 
     if (computerChoice === humanChoice) {
-        return console.log("Game tied. Lets play again.");
+        console.log(`Your chose ${humanChoice}`);
+        console.log(`Computer chose ${computerChoice}`);
+        console.log(`Game tied. Play Again 
+            `);
+    }
+    else if (computerChoice === "ROCK" && humanChoice === "PAPER") {
+        humanScore++;
+        console.log(`Your chose ${humanChoice}`);
+        console.log(`Computer chose ${computerChoice}`);
+        console.log(`Congrats! You win. ${humanChoice} beats ${computerChoice}`) ;
+        console.log(`Now your score is ${humanScore} and computer score is ${computerScore}
+            `);
+    }
+    else if (computerChoice === `ROCK` && humanChoice === `SCISSOR`){
+        computerScore++ ;
+        console.log(`Your chose ${humanChoice}`);
+        console.log(`Computer chose ${computerChoice}`);
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        console.log(`Now your score is ${humanScore} and computer score is ${computerScore}
+            `);
 
-    };
-    if (computerChoice === "ROCK" && humanChoice === "PAPER") {
-        return console.log(`Congrats! You win. ${humanChoice} beats ${computerChoice}`);
-    };
+    } 
+    else if (computerChoice === `PAPER` && humanChoice === `ROCK`){
+        computerScore++ ;
+        console.log(`Your chose ${humanChoice}`);
+        console.log(`Computer chose ${computerChoice}`);
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        console.log(`Now your score is ${humanScore} and computer score is ${computerScore}
+            `);
 
-    if (computerChoice === "ROCK" && humanChoice === "SCISSOR") {
-        return console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
-    };
-    if (computerChoice === "SCISSOR" && humanChoice === "ROCK") {
-        return console.log(`Congrats! You win. ${humanChoice} beats ${computerChoice}`);
-    };
-    if (computerChoice === "SCISSOR" && humanChoice === "PAPER") {
-        return console.log(`Congrats! You win. ${humanChoice} beats ${computerChoice}`);
-    };
-    if (computerChoice === "PAPER" && humanChoice === "ROCK") {
-        return console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-    };
-    if (computerChoice === "PAPER" && humanChoice === "SCISSOR") {
-        return console.log(`Congrats! You win. ${humanChoice} beats ${computerChoice}`);
-    };
-    if (computerChoice !== humanChoice){
-    console.log(`You entered a wrong word. Please check your spelling and try again.`) ;
-    };
-    
-}
+    } 
+    else if (computerChoice === `PAPER` && humanChoice === `SCISSOR`){
+        humanScore++;
+        console.log(`Your chose ${humanChoice}`);
+        console.log(`Computer chose ${computerChoice}`);
+        console.log(`Congrats! You win. ${humanChoice} beats ${computerChoice}`)
+        console.log(`Now your score is ${humanScore} and computer score is ${computerScore}
+            `);
 
-const humanSelection = gett;
-const computerSelection = getComputerChoice();
+    } 
+    else if (computerChoice === `SCISSOR` && humanChoice === `ROCK`){
+        humanScore++;
+        console.log(`Your chose ${humanChoice}`);
+        console.log(`Computer chose ${computerChoice}`);
+        console.log(`Congrats! You win. ${humanChoice} beats ${computerChoice}`)
+        console.log(`Now your score is ${humanScore} and computer score is ${computerScore}
+            `);
 
-playRound(humanSelection, computerSelection);
+    } 
+    else if (computerChoice === `SCISSOR` && humanChoice === `PAPER`){
+        computerScore++ ;
+        console.log(`Your chose ${humanChoice}`);
+        console.log(`Computer chose ${computerChoice}`);
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        console.log(`Now your score is ${humanScore} and computer score is ${computerScore}
+            `);
 
+    } 
+    else{
+        console.log(`Your entered ${humanChoice}`);
+        console.log(`Computer chose ${computerChoice}`);
+        console.log(`You entered a wrong word. Please check your spelling and try again.
+            `);
 
-// 3. Comparing
-//     1. if programm and user choosed the same option then both will win
-//     2. if program choose rock and user choose paper then programm will output Congrats You win.
-//     3. otherwise if user choose scissor then itll output hurray I won.
-//     4. p-scissor & u-rock = you win
-//     5. p- s & u-p = i win
-//     6. p-p & u-rock= i win 
-//     7. p-p & u-s = you win
+    } 
 
-// function giveAnswer()
-// 4. Give option to replay the game.
+} ;
 
+playRound (humanSelection , computerSelection) ;
